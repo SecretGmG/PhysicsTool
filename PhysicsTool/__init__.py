69d8c72cf4518976fd7219f3817382a9c49bcb61
SecretGmG <cedric@sigrist.ch>
2024-05-25 13:33:49 +0200

    added reversed implementations for the overloaded operators

--- PhysicsTool/__init__.py
+++ PhysicsTool/__init__.py
@@ -1,4 +1,3 @@
 from .logging import log
 from .plotting import start_plt, end_plt, err_band_plot
 from .err import Err, calc_err, derive_err
-from . import constants, imports
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PhysicsTool",
-    version="2.0",
-    packages=find_packages()
+    version="2.1",
+    packages=find_packages(),
+    requires=['numpy', 'sympy']
 )
