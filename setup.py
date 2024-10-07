5b3904b792757ebba3c310da3c165811697f24d4
SecretGmG <cedric@sigrist.ch>
2024-10-07 13:39:36 +0200

    updated setup and examples

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PhysicsTool",
-    version="3.1",
+    version="3.2",
     packages=find_packages(),
     requires=['numpy', 'sympy']
 )
