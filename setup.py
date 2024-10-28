6e28a826c7bbfb51f3c5128e3ee18da0f87aedcd
SecretGmG <cedric@sigrist.ch>
2024-10-28 17:09:51 +0100

    fixed bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PhysicsTool",
-    version="3.15",
+    version="3.16",
     packages=find_packages(),
     requires=['numpy', 'sympy']
 )
