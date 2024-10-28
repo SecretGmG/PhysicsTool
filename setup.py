c2c3a3f29099bbcd19605833cd132f21182e3265
SecretGmG <cedric@sigrist.ch>
2024-10-28 17:00:29 +0100

    changed precsion again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PhysicsTool",
-    version="3.12",
+    version="3.13",
     packages=find_packages(),
     requires=['numpy', 'sympy']
 )
