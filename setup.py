bd522aebdc52387b22b0a135ef3b00ad10b31cb1
cedric <cedric@sigrist.ch>
2025-10-06 19:18:33 +0200

    changed version nr

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages # type: ignore
 
 setup(
     name="PhysicsTool",
-    version="5.1",
+    version="5.2",
     packages=find_packages(),
     requires=["numpy", "sympy", "pandas", "matplotlib", "scipy"],
 )
